a3785ce52747de9940894a52afa0817aab64a466
Yuriy Kis <yurakis9@gmail.com>
2022-11-07 20:44:17 +0100

    Remove unused stuff from player.go
    Signed-off-by: Yuriy Kis <yurakis9@gmail.com>

--- character/player.go
+++ character/player.go
@@ -1,28 +1,7 @@
 package character
 
-import (
-	"pacman/utils"
-
-	"fyne.io/fyne/v2"
-)
-
 type Player struct {
 	character
-}
-
-func (char *Player) CharacterType() utils.CharacterType {
-	return utils.TPlayer
-}
-
-func (char *Player) InitCharacter() {
-	char.setCharacterImage()
-}
-
-func (char *Player) setCharacterImage() {
-	img, ok := utils.ResourceForCharacter(char.CharacterType()).(*fyne.StaticResource)
-	if ok {
-		char.img = img
-	}
 }
 
 func (char *Player) Move() {
